df12b63a4e3a60d57911d27e2d125589fc733101
Will Harris <will.t.harris@protonmail.com>
2021-10-23 12:43:04 -0700

    feat: fill out board with tile placeholders

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use bevy::prelude::*;
+use itertools::Itertools;
 
 const TILE_SIZE: f32 = 40.0;
+const TILE_SPACER: f32 = 10.0;
 
 struct Board {
     size: u8,
@@ -8,6 +10,7 @@ struct Board {
 
 struct Materials {
     board: Handle<ColorMaterial>,
+    tile_placeholder: Handle<ColorMaterial>,
 }
 
 impl FromWorld for Materials {
@@ -16,6 +19,7 @@ impl FromWorld for Materials {
 
         Materials {
             board: materials.add(Color::rgb(0.7, 0.7, 0.8).into()),
+            tile_placeholder: materials.add(Color::rgb(0.75, 0.75, 0.9).into()),
         }
     }
 }
@@ -35,13 +39,34 @@ fn setup(mut commands: Commands) {
 
 fn spawn_board(mut commands: Commands, materials: Res<Materials>) {
     let board = Board { size: 4 };
-    let physical_board_size = f32::from(board.size) * TILE_SIZE;
+    let physical_board_size =
+        f32::from(board.size) * TILE_SIZE + f32::from(board.size + 1) * TILE_SPACER;
 
     commands
         .spawn_bundle(SpriteBundle {
             material: materials.board.clone(),
             sprite: Sprite::new(Vec2::new(physical_board_size, physical_board_size)),
             ..Default::default()
+        })
+        .with_children(|builder| {
+            let offset = -physical_board_size * 0.5 + TILE_SIZE * 0.5;
+
+            for tile in (0..board.size).cartesian_product(0..board.size) {
+                builder.spawn_bundle(SpriteBundle {
+                    material: materials.tile_placeholder.clone(),
+                    sprite: Sprite::new(Vec2::new(TILE_SIZE, TILE_SIZE)),
+                    transform: Transform::from_xyz(
+                        offset
+                            + f32::from(tile.0) * TILE_SIZE
+                            + f32::from(tile.0 + 1) * TILE_SPACER,
+                        offset
+                            + f32::from(tile.1) * TILE_SIZE
+                            + f32::from(tile.1 + 1) * TILE_SPACER,
+                        1.0,
+                    ),
+                    ..Default::default()
+                });
+            }
         })
         .insert(board);
 }
