3b2a16d3cd2fbc98970c7ee483f5c73428409190
Will Harris <will.t.harris@protonmail.com>
2021-10-23 14:04:49 -0700

    feat: spawn starting tiles on the board
    Refactor some of the more mathy bits into impl blocks, and explicitly
    instruct bevy to only spawn starting tiles after the board setup is
    complete.

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,59 @@
 use bevy::prelude::*;
 use itertools::Itertools;
+use rand::prelude::*;
 
 const TILE_SIZE: f32 = 40.0;
 const TILE_SPACER: f32 = 10.0;
 
 struct Board {
     size: u8,
+    physical_size: f32,
+}
+
+impl Board {
+    fn new(size: u8) -> Self {
+        let physical_size = f32::from(size) * TILE_SIZE + f32::from(size + 1) * TILE_SPACER;
+
+        Board {
+            size,
+            physical_size,
+        }
+    }
+    fn cell_position_to_physical(&self, pos: u8) -> f32 {
+        let offset = -self.physical_size / 2.0 + 0.5 * TILE_SIZE;
+
+        offset + f32::from(pos) * TILE_SIZE + f32::from(pos + 1) * TILE_SPACER
+    }
 }
 
 struct Materials {
     board: Handle<ColorMaterial>,
     tile_placeholder: Handle<ColorMaterial>,
+    tile: Handle<ColorMaterial>,
 }
 
 impl FromWorld for Materials {
-    fn from_world(world: &mut World) -> Materials {
+    fn from_world(world: &mut World) -> Self {
         let mut materials = world.get_resource_mut::<Assets<ColorMaterial>>().unwrap();
 
         Materials {
             board: materials.add(Color::rgb(0.7, 0.7, 0.8).into()),
             tile_placeholder: materials.add(Color::rgb(0.75, 0.75, 0.9).into()),
+            tile: materials.add(Color::hsla(300.3, 1.0, 0.5, 0.5).into()),
         }
     }
 }
+
+struct Points {
+    value: u32,
+}
+
+struct Position {
+    x: u8,
+    y: u8,
+}
+
+struct TileText;
 
 fn main() {
     App::build()
@@ -30,6 +61,7 @@ fn main() {
         .init_resource::<Materials>()
         .add_startup_system(setup.system())
         .add_startup_system(spawn_board.system())
+        .add_startup_system_to_stage(StartupStage::PostStartup, spawn_tiles.system())
         .run()
 }
 
@@ -38,30 +70,22 @@ fn setup(mut commands: Commands) {
 }
 
 fn spawn_board(mut commands: Commands, materials: Res<Materials>) {
-    let board = Board { size: 4 };
-    let physical_board_size =
-        f32::from(board.size) * TILE_SIZE + f32::from(board.size + 1) * TILE_SPACER;
+    let board = Board::new(4);
 
     commands
         .spawn_bundle(SpriteBundle {
             material: materials.board.clone(),
-            sprite: Sprite::new(Vec2::new(physical_board_size, physical_board_size)),
+            sprite: Sprite::new(Vec2::new(board.physical_size, board.physical_size)),
             ..Default::default()
         })
         .with_children(|builder| {
-            let offset = -physical_board_size * 0.5 + TILE_SIZE * 0.5;
-
             for tile in (0..board.size).cartesian_product(0..board.size) {
                 builder.spawn_bundle(SpriteBundle {
                     material: materials.tile_placeholder.clone(),
                     sprite: Sprite::new(Vec2::new(TILE_SIZE, TILE_SIZE)),
                     transform: Transform::from_xyz(
-                        offset
-                            + f32::from(tile.0) * TILE_SIZE
-                            + f32::from(tile.0 + 1) * TILE_SPACER,
-                        offset
-                            + f32::from(tile.1) * TILE_SIZE
-                            + f32::from(tile.1 + 1) * TILE_SPACER,
+                        board.cell_position_to_physical(tile.0),
+                        board.cell_position_to_physical(tile.1),
                         1.0,
                     ),
                     ..Default::default()
@@ -69,4 +93,54 @@ fn spawn_board(mut commands: Commands, materials: Res<Materials>) {
             }
         })
         .insert(board);
+}
+
+fn spawn_tiles(mut commands: Commands, materials: Res<Materials>, query_board: Query<&Board>) {
+    let board = query_board
+        .single()
+        .expect("we always expect a board, panic if one does not exist");
+
+    let mut random_number = rand::thread_rng();
+
+    let starting_tiles: Vec<(u8, u8)> = (0..board.size)
+        .cartesian_product(0..board.size)
+        .choose_multiple(&mut random_number, 2);
+
+    for (x, y) in starting_tiles.iter() {
+        let position = Position { x: *x, y: *y };
+
+        commands
+            .spawn_bundle(SpriteBundle {
+                material: materials.tile.clone(),
+                sprite: Sprite::new(Vec2::new(TILE_SIZE, TILE_SIZE)),
+                transform: Transform::from_xyz(
+                    board.cell_position_to_physical(position.x),
+                    board.cell_position_to_physical(position.y),
+                    1.0,
+                ),
+                ..Default::default()
+            })
+            .with_children(|child_builder| {
+                child_builder
+                    .spawn_bundle(Text2dBundle {
+                        text: Text::with_section(
+                            "2",
+                            TextStyle {
+                                font_size: 40.0,
+                                color: Color::BLACK,
+                                ..Default::default()
+                            },
+                            TextAlignment {
+                                vertical: VerticalAlign::Center,
+                                horizontal: HorizontalAlign::Center,
+                            },
+                        ),
+                        transform: Transform::from_xyz(0.0, 0.0, 1.0),
+                        ..Default::default()
+                    })
+                    .insert(TileText);
+            })
+            .insert(Points { value: 2 })
+            .insert(position);
+    }
 }
